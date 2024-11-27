1b65b2b2f7fd5083387b93125a0288c6e471014c
Venkanna Dharavath <168371287+Venkanna369@users.noreply.github.com>
2024-11-27 23:49:58 +0000

    Items create, get, delete and update added in crud.py

--- app/crud.py
+++ app/crud.py
@@ -31,3 +31,35 @@ def delete_customer(id):
     deleted = cursor.rowcount > 0
     conn.close()
     return deleted
+
+# CRUD for Items
+def create_item(name, price):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO items (name, price) VALUES (?, ?)", (name, price))
+    conn.commit()
+    conn.close()
+ 
+def get_item(id):
+    conn = get_db_connection()
+    item = conn.execute("SELECT * FROM items WHERE id = ?", (id,)).fetchone()
+    conn.close()
+    return item
+ 
+def update_item(id, name, price):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE items SET name = ?, price = ? WHERE id = ?", (name, price, id))
+    conn.commit()
+    updated = cursor.rowcount > 0
+    conn.close()
+    return updated
+ 
+def delete_item(id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM items WHERE id = ?", (id,))
+    conn.commit()
+    deleted = cursor.rowcount > 0
+    conn.close()
+    return deleted
